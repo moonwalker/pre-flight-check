f396115d481496ff083b00306b3cfcc472d4111c
Vio <vio@beanon.com>
2021-06-23 16:54:02 +0200

    build: release-it - pass git tag

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-npx release-it --config ./config/release-it/release.js
+TAG=$(git describe --tags --abbrev=0)
+VERSION=${TAG:1}
+
+./node_modules/.bin/release-it $VERSION --config ./config/release-it/release.js --ci
