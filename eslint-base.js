bf53d3e82b0d8580fa2f0de0f4feb40d99796203
Bot @ Moonwalker <48728329+moonwalker-bot@users.noreply.github.com>
2021-10-26 17:20:04 +0200

    Update dependencies (#353)
    * fix(deps): update dependency prettier to v2.3.2
    
    * fix(deps): update dependency eslint to v7.30.0
    
    * fix(deps): update dependency lint-staged to v11.0.1
    
    * fix(deps): update dependency eslint to v7.31.0
    
    * fix(deps): update dependency eslint-plugin-jest to v24.3.7
    
    * fix(deps): update dependency eslint-plugin-jest to v24.4.0
    
    * fix(deps): update dependency lint-staged to v11.1.0
    
    * fix(deps): update dependency lint-staged to v11.1.1
    
    * fix(deps): update dependency eslint to v7.32.0
    
    * fix(deps): update dependency lint-staged to v11.1.2
    
    * fix(deps): update dependency eslint-import-resolver-node to v0.3.5
    
    * fix(deps): update dependency eslint-plugin-import to v2.24.0
    
    * fix(deps): update dependency eslint-import-resolver-node to v0.3.6
    
    * fix(deps): update dependency eslint-plugin-import to v2.24.1
    
    * fix(deps): update dependency eslint-plugin-prettier to v3.4.1
    
    * fix(deps): update dependency eslint-plugin-import to v2.24.2
    
    * fix(deps): update dependency eslint-plugin-react to v7.25.0
    
    * fix(deps): update dependency eslint-plugin-react to v7.25.1
    
    * fix(deps): update dependency eslint-plugin-prettier to v4
    
    * fix(deps): update dependency prettier to v2.4.0
    
    * fix(deps): update dependency eslint-plugin-react to v7.25.2
    
    * fix(deps): update dependency prettier to v2.4.1
    
    * fix(deps): update dependency eslint-plugin-jest to v24.4.2
    
    * fix(deps): update dependency eslint-plugin-react to v7.25.3
    
    * fix(deps): update dependency eslint-plugin-react to v7.26.0
    
    * fix(deps): update dependency eslint-plugin-jest to v24.4.3
    
    * fix(deps): update dependency eslint-plugin-jest to v24.5.0
    
    * fix(deps): update dependency eslint-plugin-react to v7.26.1
    
    * fix(deps): update dependency eslint-plugin-jest to v24.5.2
    
    * fix(deps): update dependency lint-staged to v11.2.0
    
    * fix(deps): update dependency lint-staged to v11.2.1
    
    * fix(deps): update dependency lint-staged to v11.2.2
    
    * fix(deps): update dependency eslint-plugin-jest to v24.6.0
    
    * fix(deps): update dependency lint-staged to v11.2.3
    
    * fix(deps): update dependency eslint-plugin-jest to v24.7.0
    
    * fix(deps): update dependency eslint-plugin-jest to v25
    
    * fix(deps): update dependency eslint-plugin-jest to v25.0.5
    
    * fix(deps): update dependency eslint-plugin-import to v2.25.2
    
    * fix(deps): update dependency eslint-plugin-jest to v25.2.0
    
    * fix(deps): update dependency eslint-plugin-jest to v25.2.1
    
    * fix(deps): update dependency eslint-plugin-jest to v25.2.2
    
    * fix(deps): update dependency eslint-import-resolver-webpack to v0.13.2
    
    * fix(deps): update dependency stylelint to v14
    
    * fix(deps): update dependency stylelint-config-recommended to v6
    
    * fix(deps): update dependency lint-staged to v11.2.4
    
    * fix(deps): update dependency stylelint-config-css-modules to v2.3.0
    
    * fix(deps): update dependency lint-staged to v11.2.5
    
    * chore(deps): update dependency release-it to v14.11.6
    
    * fix(deps): update dependency eslint to v8
    
    * chore(deps): update dependency @release-it/conventional-changelog to v3
    
    * fix: Replace eslint parser
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>
    Co-authored-by: Viorel Cojocaru <vio@beanon.com>

--- eslint-base.js
+++ eslint-base.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['airbnb-base', 'prettier'],
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module'
